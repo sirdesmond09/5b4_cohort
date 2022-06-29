e1379774550d0a43fa425eda0fc2886d1951653c
sirdesmond09 <sirdesmond09@gmail.com>
2022-06-29 15:36:58 +0100

    conditionals

---
+++ forth.py
@@ -0,0 +1,33 @@
+# def remove_chars(word, n):
+#     x = word[n:]
+#     return x
+
+
+
+
+# print(remove_chars("pynative", 2))
+
+
+# num = 37
+
+# if num > 32:
+#     print(5)
+    
+# elif num > 23:
+#     print(10)
+    
+# else:
+#     print(4)
+
+    
+# if num > 23:
+#     print(10)
+    
+    
+    
+data = int(input("Enter your age: "))
+
+if data >= 18:
+    print("You can vote")
+else:
+    print("Go back to your house. No be small children we dey find for here!")
